276a0c96e48ca9b8b90f0d49d5a1da7ad5072336
marzie Jahankhani <jahankhani1994@gmail.com>
2020-05-28 20:42:33 +0430

    Added Simple Hello World!

--- Hello World!/Program.cs
+++ Hello World!/Program.cs
@@ -10,6 +10,8 @@ namespace Hello_World_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
